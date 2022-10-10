e879b376456066569915af197d13067eece011b5
Kalash <kalash.hatwar2001@gmail.com>
2022-10-10 21:01:42 +0530

    day5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -25,8 +26,8 @@ const MyApp({super.key});
       initialRoute : "/",
       routes: {
         "/" :(context) => LoginPage(),
-        "/home" :(context) => HomePage(),
-        "/login" :(context) => LoginPage(),
+        MyRoutes.homeRoute :(context) => HomePage(),
+        MyRoutes.loginRoute :(context) => LoginPage(),
       },
     );
   }
